7c348c804fe17fc86c11dceb39285cfb29d4e658
Gustavo Passos <gusapas26@gmail.com>
2024-09-20 14:01:58 -0300

    up

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,31 @@
+import java.net.SocketOption;
+import java.sql.SQLOutput;
+import java.util.LinkedList;
 import java.util.Stack;
+import java.util.Queue;
 
 public class Test {
+    public static void main(String[] args) {
+        Queue<Integer> notas = new LinkedList<>();
 
-    public static void main (String[] args) {
+        notas.add(7);
+        notas.add(8);
+        notas.add(9);
+        notas.add(6);
+        notas.add(10);
+        notas.add(2);
+
+        System.out.println("Topo da fila: " +notas.peek());// elemento topo da pilha
+        System.out.println(notas.poll());//retirar elemento da fila
+        System.out.println(notas);
+        System.out.println(notas.isEmpty()?"Fila vazia":"Fila com elementos");//operador um ou outro
+
+
+    }
+
+
+
+    /*public static void main (String[] args) {
         Stack<Integer> notas = new Stack<>();
         notas.add(2);
         notas.add(3);
@@ -18,6 +41,5 @@ public class Test {
             System.out.println(n);
         }
     }
-
-
+    */
 }
